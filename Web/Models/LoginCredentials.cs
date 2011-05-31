e72de3221f953999ed4926b9f83f90ffbe013e54
Tatham Oddie <tatham@oddie.com.au>
2011-06-01 09:37:50 +1000

    Added field validation for login credentials.

--- Web/Models/LoginCredentials.cs
+++ Web/Models/LoginCredentials.cs
@@ -1,8 +1,16 @@
-﻿namespace Librarian.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Librarian.Web.Models
 {
     public class LoginCredentials
     {
+        [Display(Name = "tinyPM Instance URI")]
+        [Required]
         public string InstanceUri { get; set; }
+
+        [Display(Name = "API Key")]
+        [Required]
+        [StringLength(32, MinimumLength = 32, ErrorMessage = "The API Key must be 32 hexadecimal characters.")]
         public string ApiKey { get; set; }
     }
 }
