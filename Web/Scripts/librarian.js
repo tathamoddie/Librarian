4e20a5c03c6b56a7c3d50c7579be5afb5d1d3bc2
Tatham Oddie <tatham@oddie.com.au>
2011-06-01 15:21:37 +1000

    Implemented client-side color change call.

--- Web/Scripts/librarian.js
+++ Web/Scripts/librarian.js
@@ -3,29 +3,48 @@
 
 $(function () {
 
-    $("ol.backlog")
+    $('ol.backlog')
         .each(function () {
 
             var backlogTable = $(this);
 
-            var contextMenu = $("<ul id='backlog-contextMenu' class='contextMenu'></ul>");
-            contextMenu.append($("<li><a href='#white'>White</a></li>"));
-            contextMenu.append($("<li><a href='#yellow'>Yellow</a></li>"));
-            contextMenu.append($("<li><a href='#orange'>Orange</a></li>"));
-            contextMenu.append($("<li><a href='#red'>Red</a></li>"));
-            contextMenu.append($("<li><a href='#green'>Green</a></li>"));
-            contextMenu.append($("<li><a href='#blue'>Blue</a></li>"));
+            var contextMenu = $('<ul id="backlog-contextMenu" class="contextMenu"></ul>');
+            contextMenu.append($('<li><a href="#color-white">White</a></li>'));
+            contextMenu.append($('<li><a href="#color-yellow">Yellow</a></li>'));
+            contextMenu.append($('<li><a href="#color-orange">Orange</a></li>'));
+            contextMenu.append($('<li><a href="#color-red">Red</a></li>'));
+            contextMenu.append($('<li><a href="#color-green">Green</a></li>'));
+            contextMenu.append($('<li><a href="#color-blue">Blue</a></li>'));
             contextMenu.insertAfter(backlogTable);
 
             backlogTable
-                .find("li")
+                .find('li')
                 .contextMenu(
                     { menu: 'backlog-contextMenu' },
                     function (action, element) {
-                        alert(action);
+                        var storyId = $(element).data('story-id');
+
+                        var colorAction = /^color-(\w*)/i.exec(action);
+                        if (colorAction) {
+                            var newColor = colorAction[1];
+                            setStoryColor(element, storyId, newColor);
+                        }
                     }
                 );
 
         });
 
 });
+
+function setStoryColor(storyElement, storyId, newColor) {
+    storyElement = $(storyElement);
+    $.post(
+        'backlog/' + storyId + '/set-color',
+        'color=' + newColor,
+        function () {
+            var existingColorClass = /story-color-\w*/i.exec(storyElement.attr('class'));
+            if (existingColorClass) storyElement.removeClass(existingColorClass[0]);
+            storyElement.addClass('story-color-' + newColor);
+        }
+    );
+}
