f6c3efb21b9e9a83067ed43dc6bf45270b280642
Tatham Oddie <tatham@oddie.com.au>
2011-06-01 14:58:53 +1000

    Implemented context menu UI for changing card colors.

---
+++ Web/Scripts/librarian.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="jquery-1.5.1.js" />
+/// <reference path="jquery.contextMenu.js" />
+
+$(function () {
+
+    $("ol.backlog")
+        .each(function () {
+
+            var backlogTable = $(this);
+
+            var contextMenu = $("<ul id='backlog-contextMenu' class='contextMenu'></ul>");
+            contextMenu.append($("<li><a href='#white'>White</a></li>"));
+            contextMenu.append($("<li><a href='#yellow'>Yellow</a></li>"));
+            contextMenu.append($("<li><a href='#orange'>Orange</a></li>"));
+            contextMenu.append($("<li><a href='#red'>Red</a></li>"));
+            contextMenu.append($("<li><a href='#green'>Green</a></li>"));
+            contextMenu.append($("<li><a href='#blue'>Blue</a></li>"));
+            contextMenu.insertAfter(backlogTable);
+
+            backlogTable
+                .find("li")
+                .contextMenu(
+                    { menu: 'backlog-contextMenu' },
+                    function (action, element) {
+                        alert(action);
+                    }
+                );
+
+        });
+
+});
